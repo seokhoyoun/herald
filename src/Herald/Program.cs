0f3ef4d424d032a4e50ef6d5c57c2d85e7fd25e0
stone.youn <shyoun@j-onesoft.com>
2025-10-02 20:28:37 +0900

    .

--- src/Herald/Program.cs
+++ src/Herald/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MongoDB.Driver;
 namespace Herald
 {
     public class Program
@@ -5,6 +9,18 @@ namespace Herald
         public static void Main(string[] args)
         {
             var builder = Host.CreateApplicationBuilder(args);
+            // MongoDB client
+            builder.Services.AddSingleton<IMongoClient>(sp =>
+            {
+                var cfg = sp.GetRequiredService<IConfiguration>();
+                var conn = cfg.GetConnectionString("MongoDb") ?? cfg["MongoDb:ConnectionString"] ?? "mongodb://localhost:27017";
+                return new MongoClient(conn);
+            });
+
+            // Markdown monitor
+            builder.Services.AddSingleton<IMarkdownMonitor, MarkdownMonitor>();
+
+            // Worker
             builder.Services.AddHostedService<Worker>();
 
             var host = builder.Build();
